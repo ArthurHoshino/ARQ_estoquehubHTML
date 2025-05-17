4cd21ab3cc60a6974bd174cecb231c8834fcc30a
guilherme <guilhermepiantamar@gmail.com>
2025-05-16 22:14:34 -0300

    Salvando alterações antes de trocar de branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
     const mobileMenuClose = document.getElementById('mobile-menu-close');
     const mobileMenu = document.getElementById('mobile-menu');
@@ -23,8 +23,17 @@ document.addEventListener('DOMContentLoaded', function() {
     mobileMenuClose.addEventListener('click', closeMenu);
     overlay.addEventListener('click', closeMenu);
 
-    const mobileLinks = document.querySelectorAll('.mobile-menu-links a');
-    mobileLinks.forEach(link => {
-        link.addEventListener('click', closeMenu);
+    
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.aside li');
+
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            // Remove 'active' de todos
+            links.forEach(l => l.querySelector('a').classList.remove('active'));
+            // Adiciona 'active' apenas ao clicado
+            link.querySelector('a').classList.add('active');
+        });
     });
 });
